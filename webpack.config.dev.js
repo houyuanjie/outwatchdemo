817198850b5a57b2073905a51aa7c543b502bb81
houyuanjie <houyuanjie555@qq.com>
2022-02-18 14:24:42 +0800

    优化可读性，无功能更改

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,10 @@
-var webpack = require('webpack');
+let webpack = require('webpack');
 
 module.exports = require('./scalajs.webpack.config');
 
 const Path = require('path');
 const rootDir = Path.resolve(__dirname, '../../../..');
+
 module.exports.devServer = {
     contentBase: [
         Path.resolve(__dirname, 'dev'), // fastOptJS output
